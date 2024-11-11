3910ecb48e14a4a8e69061ce685736db85c8843a
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-12 03:37:55 +0600

    prisma book-member-borrow model added

---
+++ prisma/migrations/20241111213717_book_member_borrow/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "borrowRecords" ADD CONSTRAINT "borrowRecords_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "books"("bookId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrowRecords" ADD CONSTRAINT "borrowRecords_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "members"("memberId") ON DELETE RESTRICT ON UPDATE CASCADE;
