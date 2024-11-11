41a6529578b60f3b9ee63420acf6e29b5271ef96
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-12 01:58:38 +0600

    prisma member-model added

---
+++ prisma/migrations/20241111195751_member_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "members" (
+    "memberId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "membershipDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "members_pkey" PRIMARY KEY ("memberId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_email_key" ON "members"("email");
