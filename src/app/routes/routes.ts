164c65cdc66e869aa73ba485b6598b388cd423f8
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-12 00:11:12 +0600

    prisma book-model added

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,30 +1,28 @@
 import { Router } from "express";
-import { BookRoutes } from "../modules/Book/book.routes";
-import { BorrowRoutes } from "../modules/Borrow/borrow.routes";
-import { MemberRoutes } from "../modules/Member/member.routes";
-import { ReturnRoutes } from "../modules/Return/return.routes";
+// import { BookRoutes } from "../modules/Book/book.routes";
+// import { BorrowRoutes } from "../modules/Borrow/borrow.routes";
+// import { MemberRoutes } from "../modules/Member/member.routes";
+// import { ReturnRoutes } from "../modules/Return/return.routes";
 
 const router = Router();
 
-const moduleRoutes = [
-  {
-    path: "/books",
-    route: BookRoutes,
-  },
-  {
-    path: "/members",
-    route: MemberRoutes,
-  },
-  {
-    path: "/borrow",
-    route: BorrowRoutes,
-  },
-  {
-    path: "/return",
-    route: ReturnRoutes,
-  },
-];
-
-moduleRoutes.forEach((route) => router.use(route.path, route.route));
+// const moduleRoutes = [
+//   {
+//     path: "/books",
+//     route: BookRoutes,
+//   },
+//   {
+//     path: "/members",
+//     route: MemberRoutes,
+//   },
+//   {
+//     path: "/borrow",
+//     route: BorrowRoutes,
+//   },
+//   {
+//     path: "/return",
+//     route: ReturnRoutes,
+//   },
+// ];
 
 export default router;
